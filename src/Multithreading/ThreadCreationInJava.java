609f46c909f8707a8c62b9a3cdbd28679ca7dd48
Iswar Chandra Rana <ranabitu227@gmail.com>
2025-11-25 23:17:26 +0530

    Runnable Interface implementation

--- src/Multithreading/ThreadCreationInJava.java
+++ src/Multithreading/ThreadCreationInJava.java
@@ -24,5 +24,25 @@ public class ThreadCreationInJava {
 
         Thread t2 = new Thread(myRunnable);
         t2.start();
+
+
+        // Better way
+        Runnable newRunnable = () -> {
+            System.out.println("Thread is running");
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().isAlive());
+            System.out.println(Thread.currentThread().threadId());
+            System.out.println(Thread.currentThread().getPriority());
+            System.out.println(Thread.currentThread().getState());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Thread t3 = new Thread(newRunnable, "my-thread-name");
+
+        t3.start();
     }
 }
