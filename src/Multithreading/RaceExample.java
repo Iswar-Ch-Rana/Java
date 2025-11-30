691449cdb4620ae08a9c55ddb7f7f924b88dcb04
Iswar Chandra Rana <ranabitu227@gmail.com>
2025-11-30 17:45:07 +0530

    Race Example

---
+++ src/Multithreading/RaceExample.java
@@ -0,0 +1,22 @@
+package Multithreading;
+
+public class RaceExample {
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) count += 1;
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) count++;
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("Final count = " + count);
+    }
+}
